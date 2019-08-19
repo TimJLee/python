7f55fbd46aec2d45661cd634cb856b1c48b9ed3e
TimJLee <tim0409@hotmail.com>
2019-08-19 18:39:49 +0900

    2019/08/15

---
+++ intro.py
@@ -0,0 +1,10 @@
+print('Hello, World!')
+if 3>2:
+    print('it works!')
+volume=57
+if 50<volume<55:
+    print("kinda quiet")
+elif 57<=volume<60:
+    print("that's what i want to hear")
+else:
+    print("so loud")
